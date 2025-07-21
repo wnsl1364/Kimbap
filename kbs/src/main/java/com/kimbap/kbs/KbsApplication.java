6cd160184ea88d264b935e1632d6d86421bc5022
dder2013132 <dder2013@gmail.com>
2025-07-21 10:42:55 +0900

    Maven 프로젝트 초기 설정 및 기본 파일 추가

---
+++ kbs/src/main/java/com/kimbap/kbs/KbsApplication.java
@@ -0,0 +1,13 @@
+package com.kimbap.kbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbsApplication.class, args);
+	}
+
+}
---
+++ kbs/src/test/java/com/kimbap/kbs/KbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimbap.kbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
