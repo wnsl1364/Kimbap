aae7de12f4c417dba200897a7b2bb5e08029ad8d
abcals77 <abcalsgh@gmail.com>
2025-07-23 17:47:06 +0900

    2025-07-23 이민호 - 페이지 작성

--- kbs/frontend/src/router/production.js
+++ kbs/frontend/src/router/production.js
@@ -1,8 +1,34 @@
 export default [
     /** @/views/standard/products/ProductTable.vue 이런구조로*/
+    // 생산계획
     {
-        path: '/standard/products',
-        name: 'stdProd',
-        component: () => import('@/views/standard/Products.vue'),
+        path: '/production/productionPlan',
+        name: 'productionPlan',
+        component: () => import('@/views/production/ProductionPlan.vue'),
     },
+    // 생산계획목록
+    {
+        path: '/production/productionPlanList',
+        name: 'productionPlanList',
+        component: () => import('@/views/production/ProductionPlanList.vue'),
+    },
+    // 생산요청
+    {
+        path: '/production/productionRequest',
+        name: 'productionRequest',
+        component: () => import('@/views/production/ProductionRequest.vue'),
+    },
+    // 생산요청목록
+    {
+        path: '/production/productionRequestList',
+        name: 'productionRequestList',
+        component: () => import('@/views/production/ProductionRequestList.vue'),
+    },
+    // 제품입고대기
+    {
+        path: '/production/productInbound',
+        name: 'productInbound',
+        component: () => import('@/views/production/ProductInbound.vue'),
+    },
+    
 ];
