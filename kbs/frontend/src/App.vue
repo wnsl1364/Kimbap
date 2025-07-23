3b41f64609019c3da86b00ef7939b2dd034f8dfd
Jae jin <say586s@naver.com>
2025-07-23 12:39:06 +0900

    2025-07-23 심재진 로그인정보 통해서 데이터 불러오기 가능

--- kbs/frontend/src/App.vue
+++ kbs/frontend/src/App.vue
@@ -1,4 +1,19 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+import { useMemberStore } from '@/stores/memberStore'
+
+const memberStore = useMemberStore();
+
+// pinia에 사용자 정보가 없을 경우 세션 스토리지에서 가져와 동기화 해줌
+onMounted(() => {
+  if (!memberStore.user) {
+    const userStr = sessionStorage.getItem('member');
+    if (userStr) {
+      memberStore.saveUser(JSON.parse(userStr));
+    }
+  }
+});
+</script>
 
 <template>
     <router-view />
