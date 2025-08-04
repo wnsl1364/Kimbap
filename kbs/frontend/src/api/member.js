b061a1e432601b699ed8d490d448b449083789de
NugulK <gury126@gmail.com>
2025-08-04 09:11:58 +0900

    1111

---
+++ kbs/frontend/src/api/member.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+// 회원등록
+export const saveMemberAdd = (Data) => {
+  return axios.post('/api/memberAdd', Data)
+};
+
+// 사원조회
+export const cpListCheck= () => {
+  return axios.get('/api/cpList')
+};
+
+// 거래처조회
+export const empListCheck = () => {
+  return axios.get('/api/empList')
+};
+
+// 로그아웃
+export const logout = () => {
+  return axios.post('/api/logout', null, {
+    withCredentials: true
+  });
+};
+//
